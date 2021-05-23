f5123fcdfd50b7fec3375da0c16c94b33be8ab05
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-24 08:50:52 +0900

    add:FileTextReaderクラスを追加した.

---
+++ polymorphism-sample/FileTextReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_sample
+{
+    internal class FileTextReader : ITextReader
+    {
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public char Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
