fc7114c476104053f646ebec04b64929058c340e
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-24 09:17:50 +0900

    add:ReadProcess.csを追加した.

---
+++ polymorphism-sample/ReadProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_sample
+{
+    internal class ReadProcess
+    {
+        public ReadProcess(Setting setting)
+        {
+            if (setting == Setting.FILE) ReadCharacter(new FileTextReader("C:\aaa.txt"));
+            else if (setting == Setting.NETWORK) ReadCharacter(new NetworkTextReader());
+        }
+
+        public enum Setting{
+            FILE,
+            NETWORK,
+        };
+
+        private void ReadCharacter(ITextReader reader)
+        {
+            reader.Open();
+            reader.Read();
+            reader.Close();
+        }
+    }
+}
